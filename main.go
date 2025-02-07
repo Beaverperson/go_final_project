beb906d3d06db52d667a3ff4cf58b629eb10b20a
Alexander Kraynev <as.kraynev@gmail.com>
2025-02-07 11:04:48 +0300

    Add TODO_PORT variable for custom web server port

--- main.go
+++ main.go
@@ -3,14 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/Beaverperson/go_final_project/tests"
 )
 
+func getenv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
+}
+
 func main() {
-	//
+	WebPort := getenv("TODO_PORT", strconv.Itoa(tests.Port))
 	http.Handle("/", http.FileServer(http.Dir(tests.WebDir)))
-	err := http.ListenAndServe(fmt.Sprintf(":%d", tests.Port), nil)
+	err := http.ListenAndServe(":"+WebPort, nil)
 	if err != nil {
 		fmt.Printf("ошибка запуска сервера: %s\n", err.Error())
 		return
