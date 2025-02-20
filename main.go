b2ff4726c57a067abe9e29abef65ffec3c01ff3e
Alexander Kraynev <as.kraynev@gmail.com>
2025-02-20 13:19:32 +0300

    Fixed typo in DB creation schema

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
 	"os"
-	"strconv"
 
 	_ "github.com/mattn/go-sqlite3"
-
-	"github.com/Beaverperson/go_final_project/tests"
 )
 
 func getenv(key, fallback string) string {
@@ -22,17 +17,17 @@ func getenv(key, fallback string) string {
 
 func main() {
 	// setup SQL
-	db, err := getDBConnector()
+	db, err := GetDBConnector()
 	if err != nil {
 		log.Fatal()
 	}
 	defer db.Close()
 
 	// WEB
-	WebPort := getenv("TODO_PORT", strconv.Itoa(tests.Port))
-	http.Handle("/", http.FileServer(http.Dir(tests.WebDir)))
-	if err := http.ListenAndServe(":"+WebPort, nil); err != nil {
-		fmt.Printf("ошибка запуска сервера: %s\n", err.Error())
-		return
-	}
+	//WebPort := getenv("TODO_PORT", strconv.Itoa(tests.Port))
+	//http.Handle("/", http.FileServer(http.Dir(tests.WebDir)))
+	//if err := http.ListenAndServe(":"+WebPort, nil); err != nil {
+	//	fmt.Printf("ошибка запуска сервера: %s\n", err.Error())
+	//	return
+	//}
 }
--- sqlthing.go
+++ sqlthing.go
@@ -9,45 +9,38 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-var db *sql.DB
-
-func getDBConnector() (*sql.DB, error) {
+func GetDBConnector() (*sql.DB, error) {
 	appPath, err := os.Getwd()
 	if err != nil {
 		fmt.Printf("Не получилось найти текущую рабочую директорию. с каждым могло произойти")
 		return nil, err
 	}
 	dbFileName := getenv("TODO_DBFILE", "scheduler.db")
+	fmt.Printf("Имя файла для базы: %s\n", dbFileName)
 	dbFile := filepath.Join(appPath, dbFileName)
+	fmt.Printf("Полный путь к файлу базы: %s\n", dbFile)
 	_, err = os.Stat(dbFile)
-	if err == nil {
-		dbCreator, errOpen := sql.Open("sqlite3", dbFile)
-		if errOpen != nil {
-			return nil, fmt.Errorf("ошибка при открытии \"%s\"", dbFileName)
-		}
-		return dbCreator, nil
+	if err != nil {
+		os.Create(dbFile)
+		fmt.Printf("Новая база \"%s\" создана. Радостно", dbFileName)
 	}
-	os.Create(dbFile)
 	dbCreator, errOpen := sql.Open("sqlite3", dbFile)
 	if errOpen != nil {
 		return nil, fmt.Errorf("ошибка при открытии \"%s\"", dbFileName)
 	}
-	if errOpen != nil {
-		_, errCreate := dbCreator.Exec(`CREATE TABLE IF NOT EXIST "scheduler" (
-											"id"	INTEGER NOT NULL,
-											"date"	REAL NOT NULL,
-											"title"	TEXT,
-											"comment"	TEXT,
-											"repeat"	TEXT,
-											PRIMARY KEY("id" AUTOINCREMENT)
-										);
-										CREATE INDEX IF NOT EXIST indexdate ON scheduler (date);`)
-		fmt.Printf("Новая база \"%s\" создана", dbFileName)
-		if errCreate != nil {
-			return nil, errCreate
-		}
-	} else {
-		return nil, errOpen
+	fmt.Print("Подключение к базе для проверки наличия стартовой таблицы\n")
+	_, errCreate := dbCreator.Exec(`
+		CREATE TABLE IF NOT EXISTS scheduler (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		date REAL NOT NULL,
+		title TEXT,
+		comment	TEXT,
+		repeat TEXT
+		);
+		CREATE INDEX IF NOT EXISTS indexdate ON scheduler (date);
+		`)
+	if errCreate != nil {
+		return nil, errCreate
 	}
 	return dbCreator, nil
 }
